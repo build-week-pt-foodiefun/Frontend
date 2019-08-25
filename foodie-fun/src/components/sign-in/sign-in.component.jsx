adbab1c3763f3ea4e68868222b56d3f716f1a9e7
Mada Gower <gmgower@gmail.com>
2019-08-24 20:05:08 -0500

    Add Form input username & password.

--- foodie-fun/src/components/sign-in/sign-in.component.jsx
+++ foodie-fun/src/components/sign-in/sign-in.component.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import {Form, Field, withFormik } from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
+import './sign-in.styles.css'
 
 const SignIn = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <Form>
+                <label>
+                    Username: 
+                    <Field type="text" name="name" placeholder="username" />
+                </label>
+
+                <label>
+                    Password: 
+                    <Field type="email" name="password" placeholder="password"/>
+                </label>
+            </Form>
         </div>
     )
-}
+};
 
-export default SignIn;
+// Form HOC
+const FormikForm = withFormik({
+
+})(SignIn)
+
+export default FormikForm;
