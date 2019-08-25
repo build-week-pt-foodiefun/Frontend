1c428798a21d73d15f40aea528d5fd127e052055
Mada Gower <gmgower@gmail.com>
2019-08-24 19:42:14 -0500

    Add axios, formik, yup, react-router-dom & setup sign-in component files.

--- foodie-fun/src/App.js
+++ foodie-fun/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/sign-in//sign-in.component.jsx'
 
 function App() {
   return (
-    <div>
-      
+    <div className='App'>
+      <SignIn />
     </div>
   );
 }
