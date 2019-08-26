4c88fee15e706a16831f305dda79d8ecd4eba80e
Mada Gower <gmgower@gmail.com>
2019-08-25 19:18:06 -0500

    Add useState, useEffect on status to check users is there.

--- foodie-fun/src/App.js
+++ foodie-fun/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import SignIn from './components/sign-in//sign-in.component.jsx'
+import SignIn from './components/sign-in/Sign-in.jsx'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const addUser = user => {
+    setUsers([...users, user])
+  }
+
   return (
     <div className='App'>
-      <SignIn />
+      <SignIn addUser={addUser} />
+      {users.map(user => (
+        <div key={user.id}>
+           <p>Username: {user.username}</p>
+           <p>Password: {user.password}</p>
+          {/* {JSON.stringify(user)} */}
+          </div>
+      
+      ))}
+
     </div>
   );
 }
---
+++ foodie-fun/src/components/sign-in/Sign-in.jsx
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react'
+import {Form, Field, withFormik } from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
+import './sign-in.styles.css'
+
+
+
+const SignIn = (props) => {
+    console.log(props)
+    // data submit into component
+    // const [users, setUsers] = useState([])
+    const { values, touched, errors, status } = props
+    useEffect(() => {        
+        if(status){
+        props.addUser(status)
+            // setUsers([...users, status])
+        }
+    }, [status])
+
+    return (
+        <div>
+            <Form>
+                
+                {touched.username && errors.username && <p className="error">{errors.username}</p> }
+                <label>
+                    Username: 
+                    <Field type="text" name="username" placeholder="username" />
+                </label>
+
+                {touched.password && errors.password && <p className="error">{errors.password}</p> }
+                <label>
+                    Password: 
+                    <Field type="password" name="password" placeholder="password"/>
+                </label>
+                <button type="submit">Submit</button>
+                {/* {users.map(user => <div key={user.id}>{JSON.stringify(user)}</div> )} */}
+            
+            </Form>
+        </div>
+    )
+};
+
+// Form HOC
+const FormikForm = withFormik({
+    mapPropsToValues: ({ username, password }) => {
+        return {
+            username: username || '',
+            password: password || '',
+        };
+    },
+    validationSchema: yup.object().shape({
+        username: yup.string()
+        .required('Please enter you username.'),
+        password: yup.string()
+        .required('You password is required.')
+    }),
+    handleSubmit: (values, { setStatus,resetForm }) => {
+        console.log('request')
+        axios.post('https://reqres.in/api/users', values)
+        .then(res => {
+        console.log(res);
+        setStatus(res);
+        resetForm();            
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+})(SignIn)
+
+export default FormikForm;
